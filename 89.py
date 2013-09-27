5b1b611dafb9aac1390a15db0564dd16b8f8658a
Gregor Majcen <gmajcen@pixi.eu>
2013-09-27 13:26:32 +0200

    89th problem: less lines

--- 89.py
+++ 89.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python2
+import re
 roman_numbers = open('89.input').read()
-
-mapper = zip(
-    ('DCCCC', 'LXXXX', 'VIIII', 'CCCC', 'XXXX', 'IIII'),
-    ('CM', 'XC', 'IX', 'CD', 'XL', 'IV')
-)
-
-nrn = roman_numbers
-for i,d in mapper:
-    nrn = nrn.replace(i, d)
-
-print len(roman_numbers) - len(nrn)
+print len(roman_numbers) - len(re.sub(r'DCCCC|LXXXX|VIIII|CCCC|XXXX|IIII', '  ', roman_numbers))
